627432bee85ad87c55a8a65c85c505d94854e712
Rafael Aquini <aquini@redhat.com>
2012-01-30 17:18:35 -0200

    [misc] gp-kmem-footprint.sh: small adjustments.

--- gp-kmem-footprint.sh
+++ gp-kmem-footprint.sh
@@ -12,24 +12,26 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 # 
+GNUPLOT="$(which gnuplot)"
+INPUT=${1}
+DATES="$(awk '{print $1}' $INPUT | sort -u)"
+GP_SCRIPT=/tmp/gp
+DATA=/tmp/plot.dat
+
+if [ ! -x $GNUPLOT ]; then
+    echo "ERROR: gnuplot was not found!"
+    exit 1
+fi
 if [ $# -lt 1 ]; then
 	echo "USAGE: $0 <data file>"
 	echo "     - <data file>: data collected by kmem-footprint.sh script"
 	exit 1
 fi
 
-input=${1}
-dates="$(awk '{print $1}' $input | sort -u)"
-gp=/tmp/gp
-data=/tmp/plot.dat
-
-for i in $dates
-do
-	awkprog="/$(echo $i|sed -s 's/\//\\\//g' )/ {print \$2, \$3, \$4}"
-	awk "$awkprog" $input > $data
-	
+function print_gp_script()
+{
 echo "set terminal png
-set output \"kmem-footprint_$(echo $i|sed -s 's/\//-/g').png\"
+set output \"kmem-footprint_$(echo ${1}|sed -s 's/\//-/g').png\"
 set xdata time
 set timefmt \"%H:%M:%S\"
 set xrange [\"00:00:00\":\"23:59:59\"]
@@ -42,9 +44,17 @@ set format x \"%H\"
 set grid
 set key top right box
 set timestamp bottom
-set title \"Kernel Memory Footprint - $i\"
-plot \"$data\" using 1:(\$2/(2**10)) title \"usage\" with histeps lc rgb 'navy', \
-\"$data\" using 1:(\$3/(2**10)) title \"overhead\" with histeps lc rgb 'red'" > $gp
-    gnuplot $gp
-#    rm -f $cpu $gp
+set title \"Kernel Memory Footprint - ${1}\"
+plot \"$DATA\" using 1:(\$2/(2**10)) title \"usage\" with histeps lc rgb 'navy', \
+\"$DATA\" using 1:(\$3/(2**10)) title \"overhead\" with histeps lc rgb 'red'" > $GP_SCRIPT
+}
+
+
+for i in $DATES
+do
+	awkprog="/$(echo $i|sed -s 's/\//\\\//g' )/ {print \$2, \$3, \$4}"
+	awk "$awkprog" $INPUT > $DATA
+	print_gp_script "$i" 
+	$GNUPLOT $GP_SCRIPT
+	#rm -f $data $gp_script
 done
